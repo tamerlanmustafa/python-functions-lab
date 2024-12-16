ece76c4458b5acaa3dcb9962b0b29159953281ec
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-12-15 19:15:17 -0500

    Exercise 4

--- exercises.py
+++ exercises.py
@@ -48,3 +48,34 @@ print('Exercise 2:', simple_interest(1000, 5, 2))
 apply_discount = lambda price, discount_rate: int(price -(price*discount_rate/100))
 
 print('Exercise 3:', apply_discount(100, 25))
+
+# Exercise 4: Convert Temperature
+#
+# Write a function called `convert_temperature` that takes a
+# temperature and a unit ('C' for Celsius, 'F' for Fahrenheit)
+# and converts the temperature to the other unit.
+# The formula for converting Celsius to Fahrenheit is (Celsius * 9/5) + 32.
+# The formula for converting Fahrenheit to Celsius is (Fahrenheit - 32) * 5/9.
+#
+# Examples:
+# convert_temperature(0, 'C') should return 32.0.
+# convert_temperature(32, 'F') should return 0.0.
+#
+# Define the function and then call it below.
+
+# convert_temperature = lambda temp, unit: int( ((temp * 9/5 + 32) if unit == 'C' else ((temp - 32)*5/9))) lambda expression looks unreadable here
+
+
+def convert_temperature(temperature, unit):
+    if unit == 'C':
+        return (temperature * 9/5) + 32
+    elif unit == 'F':
+        return (temperature - 32) * 5/9
+    else:
+        print("error")
+
+
+
+
+print('Exercise 4: Convert 0°C to Fahrenheit:', convert_temperature(0, 'C'))
+print('Exercise 4: Convert 32°F to Celsius:', convert_temperature(32, 'F'))
